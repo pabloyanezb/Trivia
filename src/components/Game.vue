401f7792649a8d25e3699c1b321d42e4a28333a6
pabloyanezb <pabloyanezbarriga@gmail.com>
2020-08-21 23:49:07 -0400

    ADD: Preguntas en orden aleatorio

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,14 +3,14 @@
     <h1 class="my-5 white--text">Comienza a responder!</h1>
     <v-form @submit.prevent="enviar">
       <v-card
-        v-for="question in questions.slice(0,1)"
-        :key="question.id"
+        v-for="pregunta in preguntasDesordenadas.slice(0,1)"
+        :key="pregunta.id"
         class="mb-5 px-5 pt-2"
       >
-        <h3>{{question.text}}</h3>
+        <h3>{{pregunta.text}}</h3>
         <v-radio-group v-model="respuesta1">
           <v-radio
-            v-for="(answer, i) in question.answers"
+            v-for="(answer, i) in pregunta.answers"
             :key="i"
             :label="answer.answer"
             :value="answer.isCorrect ? 'ok' : answer.answer"
@@ -19,14 +19,14 @@
         </v-radio-group>
       </v-card>
       <v-card
-        v-for="question in questions.slice(1,2)"
-        :key="question.id"
+        v-for="pregunta in preguntasDesordenadas.slice(1,2)"
+        :key="pregunta.id"
         class="mb-5 px-5 pt-2"
       >
-        <h3>{{question.text}}</h3>
+        <h3>{{pregunta.text}}</h3>
         <v-radio-group v-model="respuesta2">
           <v-radio
-            v-for="(answer, i) in question.answers"
+            v-for="(answer, i) in pregunta.answers"
             :key="i"
             :label="answer.answer"
             :value="answer.isCorrect ? 'ok' : answer.answer"
@@ -35,14 +35,14 @@
         </v-radio-group>
       </v-card>
       <v-card
-        v-for="question in questions.slice(2,3)"
-        :key="question.id"
+        v-for="pregunta in preguntasDesordenadas.slice(2,3)"
+        :key="pregunta.id"
         class="mb-5 px-5 pt-2"
       >
-        <h3>{{question.text}}</h3>
+        <h3>{{pregunta.text}}</h3>
         <v-radio-group v-model="respuesta3">
           <v-radio
-            v-for="(answer, i) in question.answers"
+            v-for="(answer, i) in pregunta.answers"
             :key="i"
             :label="answer.answer"
             :value="answer.isCorrect ? 'ok' : answer.answer"
@@ -75,10 +75,16 @@ export default {
   data() {
     return {
       valido: true,
-      questions: [],
       respuesta1: '',
       respuesta2: '',
-      respuesta3: ''
+      respuesta3: '',
+    }
+  },
+  computed: {
+    preguntasDesordenadas() {
+      let preguntas = [...this.questions]
+      preguntas.sort(() => 0.5 - Math.random());
+      return preguntas;
     }
   },
   firestore() {
@@ -102,11 +108,14 @@ export default {
       const fecha = new Date();
       const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute:'2-digit', hour12: true};
       const fechaActual =  fecha.toLocaleDateString("es-CL", options);
+      
+      let porcentaje = (correctas / 3) * 100;
+      porcentaje = porcentaje.toFixed(1);
 
       db.collection('puntajes').add ({
         nombre: this.$store.state.user.displayName,
         correctas: correctas + '/3',
-        porcentaje: (correctas / 3) * 100 + '%',
+        porcentaje: porcentaje + '%',
         fechaActual: fechaActual,
         fecha: fecha
       })
